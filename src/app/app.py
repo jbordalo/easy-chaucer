c95108408d8262f82607f93ce19412ddd9bc1cb6
jbordalo <j.bordalo@campus.fct.unl.pt>
2025-01-08 23:09:14 +0000

    Add controls for controlling font size and family in the app

--- src/app/app.py
+++ src/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 lines = [
     "Whan that Aprill with <dfn data-note=\"its\">his</dfn> shoures soote",
     "The <dfn data-note=\"dryness\">droghte</dfn> of March hath perced to the roote",
-    "And bathed every veyne in swich licour",
+    "And bathed every veyne in <dfn data-note=\"such liquid\">swich licour</dfn>",
     "Of which vertu engendred is the flour;",
     "Whan Zephirus eek with his sweete breeth",
     "Inspired hath in every holt and heeth",
