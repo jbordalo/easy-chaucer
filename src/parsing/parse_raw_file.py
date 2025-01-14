8703d3b2527bdd4b23e476f12d7a1de980d8aa9f
jbordalo <j.bordalo@campus.fct.unl.pt>
2025-01-14 09:23:48 +0000

    Refactor script for parsing raw file

--- src/parsing/parse_raw_file.py
+++ src/parsing/parse_raw_file.py
@@ -1,11 +1,28 @@
 import re
 
-chapter="knights-tale"
-offset=859
-
 
 def is_verse(line):
     return not re.match(r'^\d+[\.:]?.*', line)
+
+
+#pre-condition: not is_verse
+def is_simple_note(line):
+    assert not is_verse(line)
+    try:
+        int(line.split(maxsplit=1)[0])
+        return True
+    except ValueError:
+        return False
+
+
+def is_multiline_note(line):
+    try:
+        start, end = line.split(maxsplit=1)[0].split('-')
+        int(start)
+        int(end)
+        return True
+    except Exception:
+        return False
 
 
 def expand_note(verse: str, line: str):
@@ -44,7 +61,7 @@ def expand_note(verse: str, line: str):
 
 def parse_multiline_notes(line, colon_position, start, end):
     # All on the right is surely the note
-    note_text = line[colon_position+1:].strip()
+    note_text = line[colon_position + 1:].strip()
 
     new_line = line[:colon_position].strip()
     word_by_word = new_line.split()[1:]
@@ -68,58 +85,65 @@ def parse_multiline_notes(line, colon_position, start, end):
 lines = []
 notes: dict = {}
 
-with open(f'raw_{chapter}.txt', 'r') as f:
-    """
-        For normal notes you might have ellipsis, which will make the algorithm fail
-        For multiline notes
-        If n-n+1 interpret the whole thing as 1, likely split at Upper case word, since new verse
-        If no note found (no colon), it's an explanation, add it to the first verse
-    """
 
-    for line in f:  
-        if is_verse(line):
-            lines.append(line)
-        else:
-            try:
+def get_multiline_range(start_str: str, end_str: str):
+    start = int(start_str)
+    end = int(end_str)
+
+    # For cases such as: '1025-29'
+    if end < start:
+        end = int(start_str[:-2] + end_str)
+
+    return start - offset + 1, end - offset + 1
+
+
+if __name__ == '__main__':
+    chapter="knights-tale"
+    offset=859
+
+    with open(f'raw_{chapter}.txt', 'r') as f:
+        """
+        Line can be a normal line, a simple note, a multiline note
+        """
+        for line in f:
+            if is_verse(line):
+                lines.append(line)
+            elif is_simple_note(line):
                 note_number = int(line.split(maxsplit=1)[0])
-                note_number = note_number-offset+1
+                note_number = note_number - offset + 1
                 exp_notes = expand_note(lines[note_number-1], line)
                 if not note_number in notes:
                     notes[note_number] = []
                 notes[note_number] += exp_notes[::-1]
-            except ValueError:
-                # In this case, the note is multiline
+            elif is_multiline_note(line):
                 start_str, end_str = line.split(maxsplit=1)[0].split('-')
-                start = int(start_str)-offset+1
-                end = int(end_str)
-                if end < start:
-                    end = int(start_str[:-2] + end_str)
-                end = end - offset+1
+                start, end = get_multiline_range(start_str, end_str)
+
                 colon_position = line.find(":")
                 if not end in notes:
                     notes[end] = []
 
-                # If n-n+1 interpret the whole thing as 1, likely split at Upper case word, since new verse
                 if start == end-1 and colon_position != -1:
                     parse_multiline_notes(line, colon_position, start=start, end=end)
                 else:
                     notes[end].append({
-                        "span":"",
+                        "span": "",
                         "note_text": line.split(maxsplit=1)[1].strip()
-                    })
-            except IndexError:
-                print(line)
-                exit(0)        
-
-with open(f'{chapter}.txt', 'w') as f:
-    for line in lines:
-        f.write(line)
-
-with open(f'{chapter}_notes.txt', 'w') as f:
-    for note_number in notes.keys():
-        for note in notes.get(note_number):
-            if not note['span']:
-                f.write(f"{note_number} {note['note_text']}")
+                    })     
             else:
-                f.write(f"{note_number} {note['span']}: {note['note_text']}")
-            f.write("\n")
+                # Unexpected behavior
+                print("Couldn't find type of line for line:")
+                print(line)
+
+    with open(f'{chapter}.txt', 'w') as f:
+        for line in lines:
+            f.write(line)
+
+    with open(f'{chapter}_notes.txt', 'w') as f:
+        for note_number in notes.keys():
+            for note in notes.get(note_number):
+                if not note['span']:
+                    f.write(f"{note_number} {note['note_text']}")
+                else:
+                    f.write(f"{note_number} {note['span']}: {note['note_text']}")
+                f.write("\n")
